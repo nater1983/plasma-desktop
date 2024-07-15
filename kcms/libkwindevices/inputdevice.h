Sonic-DE/sonic-desktop-interface

[kcms/tablet] Add combined tablet model
KWin/libinput splits tablet into separate tool and pad devices for technical reasons,
and we expose that verbatim in the UI.

That leads to poor UX since for the user it is one physical device

This adds a model that combines devices that belong together into one logical device

This is done using the libinput device group property, which was created specifically
for this purpose

kcms/libkwindevices/inputdevice.h
@@ -98,6 +98,21 @@ public:
     }
     void setMapToWorkspace(bool mapToWorkspace);
 
+    quint64 deviceGroup() const
+    {
+        return m_deviceGroup.value();
+    }
+
+    bool tabletPad() const
+    {
+        return m_tabletPad.value();
+    }
+
+    bool tabletTool() const
+    {
+        return m_tabletTool.value();
+    }
+
 Q_SIGNALS:
     void needsSaveChanged();
 
@@ -225,6 +240,9 @@ private:
 
     Prop<bool> m_mapToWorkspace =
         Prop<bool>(this, "mapToWorkspace", &OrgKdeKWinInputDeviceInterface::defaultMapToWorkspace, nullptr, &InputDevice::mapToWorkspaceChanged);
+    Prop<quint64> m_deviceGroup = Prop<quint64>(this, "deviceGroup");
+    Prop<bool> m_tabletPad = Prop<bool>(this, "tabletPad");
+    Prop<bool> m_tabletTool = Prop<bool>(this, "tabletTool");
 
     std::unique_ptr<OrgKdeKWinInputDeviceInterface> m_iface;
 };

nater1983/plasma-desktop

kcms/libkwindevices/inputdevice.h
@@ -117,6 +117,21 @@ public:
     }
     void setMapToWorkspace(bool mapToWorkspace);
 
+    QString deviceGroup() const
+    {
+        return m_deviceGroup.value();
+    }
+
+    bool tabletPad() const
+    {
+        return m_tabletPad.value();
+    }
+
+    bool tabletTool() const
+    {
+        return m_tabletTool.value();
+    }
+
 Q_SIGNALS:
     void needsSaveChanged();
 
@@ -244,6 +259,9 @@ private:
 
     Prop<bool> m_mapToWorkspace =
         Prop<bool>(this, "mapToWorkspace", &OrgKdeKWinInputDeviceInterface::defaultMapToWorkspace, nullptr, &InputDevice::mapToWorkspaceChanged);
+    Prop<QString> m_deviceGroup = Prop<QString>(this, "deviceGroup");
+    Prop<bool> m_tabletPad = Prop<bool>(this, "tabletPad");
+    Prop<bool> m_tabletTool = Prop<bool>(this, "tabletTool");
 
     Prop<bool> m_supportsCalibrationMatrix =
         Prop<bool>(this, "supportsCalibrationMatrix", nullptr, &OrgKdeKWinInputDeviceInterface::supportsCalibrationMatrix, nullptr);